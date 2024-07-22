36f0eb53953652d4c28b464e5176c8065c3c3d12
NoCai <1154266678@qq.com>
2024-07-22 17:11:48 +0800

    11.3梯度下降

---
+++ 11.优化算法/11.3梯度下降.py
@@ -0,0 +1,50 @@
+# 梯度下降
+#   最简单的迭代求解算法
+#   选取开始点x_0
+#   对t = 1, ..., T
+#       x_t = X_{t-1} - η * ∇f(x_{t-1})
+#       当前点 减去 学习率 乘以 梯度
+#       对每个样本都计算梯度, 然后求平均
+#   η是学习率
+
+import numpy as np
+import torch
+from d2l import torch as d2l
+
+def train_2d(trainer, steps=20, f_grad=None):  #@save
+    """用定制的训练机优化2D目标函数"""
+    # s1和s2是稍后将使用的内部状态变量
+    x1, x2, s1, s2 = -5, -2, 0, 0
+    results = [(x1, x2)]
+    for i in range(steps):
+        if f_grad:
+            x1, x2, s1, s2 = trainer(x1, x2, s1, s2, f_grad)
+        else:
+            x1, x2, s1, s2 = trainer(x1, x2, s1, s2)
+        results.append((x1, x2))
+    print(f'epoch {i + 1}, x1: {float(x1):f}, x2: {float(x2):f}')
+    return results
+
+def show_trace_2d(f, results):  #@save
+    """显示优化过程中2D变量的轨迹"""
+    d2l.set_figsize()
+    d2l.plt.plot(*zip(*results), '-o', color='#ff7f0e')
+    x1, x2 = torch.meshgrid(torch.arange(-5.5, 1.0, 0.1),
+                          torch.arange(-3.0, 1.0, 0.1), indexing='ij')
+    d2l.plt.contour(x1, x2, f(x1, x2), colors='#1f77b4')
+    d2l.plt.xlabel('x1')
+    d2l.plt.ylabel('x2')
+    d2l.plt.show()
+
+def f_2d(x1, x2):  # 目标函数
+    return x1 ** 2 + 2 * x2 ** 2
+
+def f_2d_grad(x1, x2):  # 目标函数的梯度
+    return (2 * x1, 4 * x2)
+
+def gd_2d(x1, x2, s1, s2, f_grad):
+    g1, g2 = f_grad(x1, x2)
+    return (x1 - eta * g1, x2 - eta * g2, 0, 0)
+
+eta = 0.1
+show_trace_2d(f_2d, train_2d(gd_2d, f_grad=f_2d_grad))
